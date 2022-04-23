5ff05b7906b27b65670017d1126e113e5599d100
facufrancou <francou.facundo@gmail.com>
2022-04-23 19:03:21 -0300

    Subida de templates para inicio

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static('public'));
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000/");
+});
+
+app.get("/", (req, res) => {
+    let htmlPath = path.resolve(__dirname, "./views/index.html");
+    res.sendFile(htmlPath);
+  });
+
+
+            
