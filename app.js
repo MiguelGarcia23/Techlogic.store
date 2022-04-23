768120ffa59326c9fd5e97041a6690e7461a36aa
facufrancou <francou.facundo@gmail.com>
2022-04-23 19:11:18 -0300

    reemplazo de archivos styles.css y home.html

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/index.html");
+    let htmlPath = path.resolve(__dirname, "./views/home.html");
     res.sendFile(htmlPath);
   });
 
