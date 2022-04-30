475d8f32f9a9b70da03b9f1f985769a3b4ab1202
facufrancou <facundo.francou@gmail.com>
2022-04-30 11:53:43 -0300

    modificacion de style.css

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000/");
 });
 
 app.get("/", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/home.html");
-    res.sendFile(htmlPath);
-  });
-
-
-            
+  let htmlPath = path.resolve(__dirname, "./views/index.html");
+  res.sendFile(htmlPath);
+});
+app.get("/productCart", (req, res) => {
+  let htmlPath = path.resolve(__dirname, "./views/productCart.html");
+  res.sendFile(htmlPath);
+});
