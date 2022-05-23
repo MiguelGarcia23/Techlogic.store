714797a2297b4b593813ecd4b99c8734e57e6d71
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-05-23 12:24:58 -0400

    Volviendo dinámicas las secciones best sellers y collections del index.ejs y realizando los cambios en el mainController.js

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,46 +1,85 @@
+/* Creamos un array que contenga los productos que se mostrarán en la sección best sellers */
+const bestSellers = [
+  {
+    name: 'iPhone 13',
+    price: '$234.500,00',
+    img: 'iphone13.png'
+  },
+
+  {
+    name: 'Mouse Logitech G603',
+    price: '$6.435,00',
+    img: 'mouse-logi.png'
+  },
+
+  {
+    name: 'Macbook Pro',
+    price: '$320.000,00',
+    img: 'macbook-pro.png'
+  },
+
+  {
+    name: 'Audífonos Beats Pro',
+    price: '$48.000,00',
+    img: 'audifonos-beats-pro.png'
+  },
+
+  {
+    name: 'iPad Air',
+    price: '$79.230,00',
+    img: 'ipad-air.png'
+  },
+
+  {
+    name: 'Playstation 5',
+    price: '$61.700,00',
+    img: 'playstation-5.png'
+  },
+
+  {
+    name: 'Teclado Logitech G213',
+    price: '$5.800,00',
+    img: 'teclado-logitech-g213.png'
+  },
+
+  {
+    name: 'Router TP-Link TL-MR6400',
+    price: '$8.020,00',
+    img: 'router-tplink-mr6400.png'
+  },
+];
+
+/* Creamos un array que contenga las colecciones que se mostrarán en la sección collections */
+const collections = [
+  {
+    name: 'Modern',
+    classCollection: 'modern-collection'
+  },
+
+  {
+    name: 'Classic',
+    classCollection: 'classic-collection'
+  },
+
+  {
+    name: 'Colorful',
+    classCollection: 'colorful-collection'
+  },
+
+  {
+    name: 'Black & White',
+    classCollection: 'black-white-collection'
+  }
+]
+
+
 /* Configuramos el controlador */
 const mainController = {
 
     index: (req, res) => {
-        res.render('./main/index')
+        res.render('./main/index', {bestSellers: bestSellers, collections: collections})
     }
 }
 
 /* Exportamos el controlador */
 module.exports = mainController;
-
-/* Indicando cuando me mostrará el home de la página */
-/* app.get("/", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/index.html");
-    res.sendFile(htmlPath);
-  }); */
-  
-  /* Indicando cuando me mostrará el detalle del producto en la página */
-/*   app.get("/productDetail", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/productDetail.html");
-    res.sendFile(htmlPath);
-  }); */
-  
-  /* Indicando cuando me mostrará el carrito de compras en la página */
-/*   app.get("/productCart", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/productCart.html");
-    res.sendFile(htmlPath);
-  }); */
-  
-  /* Indicando cuando me mostrará el login de la página */
-/*   app.get("/login", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/login.html");
-    res.sendFile(htmlPath);
-  }); */
-  
-  /* Indicando cuando me mostrará el register de la página */
-/*   app.get("/register", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/register.html");
-    res.sendFile(htmlPath);
-  }); */
-  
-  /* Redireccionando todos los botones al home de la página */
-/*   app.post("/", (req,res) => {
-    let htmlPath = path.resolve(__dirname, "./views/index.html");
-    res.sendFile(htmlPath);
-  }) */
