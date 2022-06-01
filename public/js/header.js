e2ffefa6f400a17fe9a09c40ee574296b0ad5cf1
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-06-01 12:57:46 -0400

    Reparando el header en header.ejs y header.css, reparando y agregando las imágenes del banner principal del index y agregando la lógica del cambio del fondo del header en el header.js

---
+++ public/js/header.js
@@ -0,0 +1,38 @@
+(function () {
+
+    /* Objeto con las propiedades */
+    let propHeader = {
+
+        header: document.getElementById('main-header'),
+        position: window.pageYOffset
+    }
+
+    /* Objeto con los métodos */
+    let methodHeader = {
+
+        inicio: () => {
+
+            window.addEventListener('scroll', methodHeader.changeHeader)
+            
+        },
+        
+        changeHeader: () => {
+
+            propHeader.position= window.pageYOffset;
+
+            if (propHeader.position > 0) {
+
+                propHeader.header.style.backgroundColor = 'black';
+                
+            } else {
+                
+                propHeader.header.style.backgroundColor = 'transparent';
+
+            }
+
+        }
+    }
+
+    methodHeader.inicio()
+
+}())
