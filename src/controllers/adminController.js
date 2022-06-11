ea4ef51e6c07aef25b0f8d115942f8dc663f4ae3
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-06-11 11:43:20 -0400

    Creando la ruta POST del formulario de creación, configurando su método en el controlador, creando la carpeta middlewares y agregando el middleware Multer

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const productsFilePath = path.join(__dirname, "../data/products.json");
 
-
 /* Configuramos el controlador */
 const adminController = {
 
@@ -13,6 +12,47 @@ const adminController = {
 
     create: (req, res) => {
         res.render('./admin/createProduct')
+    },
+
+    processCreation: (req, res, next) => {
+        const productsFile = fs.readFileSync(productsFilePath, "utf-8");
+
+        let products;
+
+        if (products == '') {
+
+            products = [];
+
+        } else {
+
+            products = JSON.parse(productsFile);
+
+        }
+
+        let newProduct = {
+            id: products[products.length - 1].id + 1,
+            ...req.body,
+            description: req.body.description,
+            price: req.body.price,
+            state: 'stock'
+        };
+
+        /* Preguntado si el usuario subió una imagen. Si no lo hizo, aparece un error  */
+		if (!req.file) {
+
+			const error = new Error ('Por favor seleccione un archivo');
+			error.httpStatusCode = 400;
+			return next(error)
+
+		} else {
+			newProduct.image = req.file.filename;
+		}
+
+        products.push(newProduct);
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+
+        res.redirect('../products')
     },
 
     edit: (req, res) => {
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -7,11 +7,17 @@ const router = express.Router();
 /* Importamos el controlador admin */
 const adminController = require ('../controllers/adminController');
 
+/* Importamos el middleware Multer */
+const multerMiddleware = require('../middlewares/multerMiddleware');
+
 /* Configuramos el envío a la home de admin */
 router.get ('/', adminController.index);
 
 /* Configuramos el envío a la sección de creación de productos de admin */
 router.get ('/create', adminController.create);
+
+/* Configuramos la ruta de envío del formulario de creación de un producto una vez completo */
+router.post ('/create', multerMiddleware.single('image') , adminController.processCreation);
 
 /* Configuramos el envío a la sección de edición de productos de admin */
 router.get ('/edit/:id', adminController.edit);
