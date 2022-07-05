1902a7d4e97bed6dfca094531dc6a9d462b1f7f2
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-07-05 15:45:17 -0400

    Eliminando el adminRouter y adminController, y pasando las rutas y funcionalidades a productsRouter y productsController respectivamente

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -92,11 +92,138 @@ const productsController = {
     }); // MANDO LOS PRODUCTOS CON ESTADO 'CART' A LA VISTA
   },
 
-  editProduct: (req, res) => {
-    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-    let productToEdit = products.filter((product) => req.params.id == product.id); // Buscamos el producto en el array de productos
+  create: (req, res) => {
+    let userLoggedAdmin = req.session.userLogged;
 
-    res.render("admin/editProduct", {product: productToEdit});
+    if (userLoggedAdmin && userLoggedAdmin.rol == "admin") {
+      // si el usuario está logueado y es admin
+      res.render("./admin/createProduct", {
+        // renderizamos la página de creación de productos
+        user: req.session.userLogged,
+      });
+    } else {
+      res.redirect("/users/login"); // si no está logueado, redirigimos al login
+    }
+  },
+
+  store: (req, res, next) => {
+    /* Leyendo el JSON */
+    const productsFile = fs.readFileSync(productsFilePath, "utf-8");
+
+    /* Estableciendo una variable que será el array de todos los productos los productos */
+    let products;
+
+    /* Preguntar si hay algun producto. Si no lo hay, que cree un array que los almacene, si sí hay productos, que transforme el JSON a un array */
+    if (products == "") {
+      products = [];
+    } else {
+      products = JSON.parse(productsFile);
+    }
+
+    /* Configurando la información del nuevo producto con lo que llenó la persona en el formulario */
+    let newProduct = {
+      id: products[products.length - 1].id + 1,
+      ...req.body,
+      description: req.body.description,
+      price: req.body.price,
+      discount: req.body.discount,
+      state: "stock",
+    };
+
+    /* Preguntado si el usuario subió una imagen. Si no lo hizo, aparece un error  */
+    if (!req.file) {
+      const error = new Error("Por favor seleccione un archivo");
+      error.httpStatusCode = 400;
+      return next(error);
+    } else {
+      newProduct.image = req.file.filename;
+    }
+
+    /* Agregando el nuevo producto al array de productos */
+    products.push(newProduct);
+
+    /* Convirtiendo el array a un JSON y reescribiendo el JSON con los nuevos productos */
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+
+    /* Redirigiendo a la lista de productos luego de enviar el formulario */
+    res.redirect("/products/");
+  },
+
+  edit: (req, res) => {
+
+    let userLoggedAdmin = req.session.userLogged;
+
+    if (userLoggedAdmin && userLoggedAdmin.rol == "admin") {// Si el usuario está logueado y es admin
+
+      const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+      let productToEdit = products.find((product) => req.params.id == product.id );
+
+      res.render("admin/editProduct", { // Renderiza la vista de editar producto
+        product: productToEdit,
+        user: req.session.userLogged,
+      });
+    } else {
+      res.redirect("/users/login"); // Si no, redirige a la página de login
+    }
+  },
+
+  update: (req, res) => {
+    /* Leyendo el JSON y convirtiéndolo en un array */
+    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+    /* Encontrando el producto seleccionado por el usuario */
+    let productToEdit = products.find((product) => product.id == req.params.id);
+
+    /* Configurando la nueva info del producto luego de los cambios del usuario */
+    productToEdit.id = req.params.id;
+    productToEdit.name = req.body.name;
+    productToEdit.section = req.body.section;
+    productToEdit.collection = req.body.collection;
+    productToEdit.brand = req.body.brand;
+    productToEdit.state = req.body.state;
+    productToEdit.description = req.body.description;
+    productToEdit.price = req.body.price;
+    productToEdit.discount = req.body.discount;
+    productToEdit.image = req.file ? req.file.filename : productToEdit.image;
+
+    /* Encontrando el index del producto editado para no tener que reescribir todo el JSON, sólo el producto editado */
+    let indexProductEdited = products.findIndex(
+      (product) => product.id == req.params.id
+    );
+
+    /* Cambiando la info del producto anterior por la nueva agregada por el usuario */
+    products[indexProductEdited] = productToEdit;
+
+    /* Convirtiendo el array a un JSON y reescribiendo el JSON con los nuevos productos */
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+
+    /* Redirigiendo a la lista de productos luego de enviar el formulario */
+    res.redirect("/products/", { user: req.session.userLogged });
+  },
+
+  delete: (req, res) => {
+    /* Leyendo el JSON y convirtiéndolo en un array */
+    let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+    /* Encontrando el producto seleccionado por el usuario */
+    let productToDelete = products.find((product) => product.id == req.params.id);
+
+    /* Eliminando el producto cambiando la propiedad deleted a true */
+    productToDelete.deleted = true;
+
+    /* Encontrando el index del producto eliminado para no tener que reescribir todo el JSON, sólo el producto eliminado */
+    let indexProductEdited = products.findIndex(
+      (product) => product.id == req.params.id
+    );
+
+    /* Cambiando el valor de la propiedad deleted */
+    products[indexProductEdited] = productToDelete;
+
+    /* Convirtiendo el array a un JSON y reescribiendo el JSON con el cambio en el producto eliminado */
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+
+    /* Redirigiendo a la lista de productos luego de enviar el formulario */
+    res.redirect("/products");
   }
 };
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -7,6 +7,9 @@ const router = express.Router();
 /* Importamos el controlador de productos */
 const productsController = require ('../controllers/productsController');
 
+/* Importamos el middleware Multer */
+const multerMiddleware = require('../middlewares/uploadProductImage');
+
 /* Configuramos el envío a la página principal de productos */
 router.get ('/', productsController.index);
 
@@ -33,6 +36,21 @@ router.post ('/productCart/:id', productsController.deleteProductToCart);
 
 /* Configuramos la ruta de los botones 'Añadir al carrito' de la lista de productos y del detalle */
 router.post ('/addProduct/:id', productsController.addToCart);
+
+/* Configuramos el envío a la sección de creación de productos de admin */
+router.get ('/create', productsController.create);
+
+/* Configuramos la ruta de envío del formulario de creación de un producto una vez completo */
+router.post ('/create', multerMiddleware.single('image') , productsController.store);
+
+/* Configuramos el envío a la sección de edición de productos de admin */
+router.get ('/edit/:id', productsController.edit);
+
+/* Configuramos la ruta de envío del formulario de edición de un producto una vez completo */
+router.put ('/edit/:id', multerMiddleware.single('image') , productsController.update);
+
+/* Configuramos la ruta para eliminar un producto */
+router.delete ('/delete/:id', productsController.delete);
 
 
 /* Exportamos la variable router */
