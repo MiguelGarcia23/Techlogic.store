b1c19aebae75d11c4d982527bb791b115893ff23
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-09-03 09:01:46 -0400

    Cambios en el navbar.js y el searchBar.js

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -1,11 +1,10 @@
-    let iconoLupaBusqueda = document.querySelector('.icono-search-right-navbar');
-    let barraBusqueda = document.querySelector('.search-bar');
-    let iconoXBarraBusqueda = document.querySelector('.button-X-search-bar')
-    
-    iconoLupaBusqueda.addEventListener('click', () => {
-        barraBusqueda.style.display = 'block'
-    })
-    iconoXBarraBusqueda.addEventListener('click', () => {
-        barraBusqueda.style.display = 'none'
-    })
+let iconoLupaBusqueda = document.querySelector(".icono-search-right-navbar");
+let barraBusqueda = document.querySelector(".search-bar");
+let iconoXBarraBusqueda = document.querySelector(".button-X-search-bar");
 
+iconoLupaBusqueda.addEventListener("click", () => {
+  barraBusqueda.style.display = "block";
+});
+iconoXBarraBusqueda.addEventListener("click", () => {
+  barraBusqueda.style.display = "none";
+});
