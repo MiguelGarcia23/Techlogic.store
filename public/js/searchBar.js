1dbf320de12c9c80c4ad71bb43b04c0cc84b34ff
facufrancou <facundo.francou@gmail.com>
2022-09-03 20:00:48 -0300

    mas estilos

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -2,9 +2,12 @@ let iconoLupaBusqueda = document.querySelector(".icono-search-right-navbar");
 let barraBusqueda = document.querySelector(".search-bar");
 let iconoXBarraBusqueda = document.querySelector(".button-X-search-bar");
 
+
 iconoLupaBusqueda.addEventListener("click", () => {
   barraBusqueda.style.display = "block";
 });
 iconoXBarraBusqueda.addEventListener("click", () => {
   barraBusqueda.style.display = "none";
+  
+
 });
